64468dfebe128ca729db534506e274566e8b8f51
Fabiano Ramos dos Santos <fabiano.santos@selbetti.com.br>
2017-04-28 18:45:40 -0300

    Initial commit.

---
+++ app.js
@@ -0,0 +1,49 @@
+// express handles the routing and serves up the index.html page and static files to visitors
+var express = require('express');
+var app = express();
+var path = require('path');
+// formidable will parse the incoming form data (the uploaded files)
+var formidable = require('formidable');
+// fs module will be used to rename uploaded files
+var fs = require('fs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+app.post('/upload', function(req, res){
+
+  // create an incoming form object
+  var form = new formidable.IncomingForm();
+
+  // specify that we want to allow the user to upload multiple files in a single request
+  form.multiples = true;
+
+  // store all uploads in the /uploads directory
+  form.uploadDir = path.join(__dirname, '/uploads');
+
+  // every time a file has been uploaded successfully,
+  // rename it to it's orignal name
+  form.on('file', function(field, file) {
+    fs.rename(file.path, path.join(form.uploadDir, file.name));
+  });
+
+  // log any errors that occur
+  form.on('error', function(err) {
+    console.log('An error has occured: \n' + err);
+  });
+
+  // once all the files have been uploaded, send a response to the client
+  form.on('end', function() {
+    res.end('success');
+  });
+
+  // parse the incoming request containing the form data
+  form.parse(req);
+});
+
+var server = app.listen(3000, function(){
+  console.log('Server listening on port 3000');
+});
